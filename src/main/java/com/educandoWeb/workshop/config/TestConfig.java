7864f9e409d201645ab972a9e9a67677b7ab364b
Edemilson das Mata <edemilsondell@hotmail.com>
2020-05-09 19:01:53 -0400

    Category

--- src/main/java/com/educandoWeb/workshop/config/TestConfig.java
+++ src/main/java/com/educandoWeb/workshop/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoWeb.workshop.entities.Category;
 import com.educandoWeb.workshop.entities.Order;
 import com.educandoWeb.workshop.entities.User;
 import com.educandoWeb.workshop.entities.enums.OrderStatus;
+import com.educandoWeb.workshop.repositories.CategoryRepository;
 import com.educandoWeb.workshop.repositories.OrderRepository;
 import com.educandoWeb.workshop.repositories.UserRepository;
 
@@ -21,10 +23,18 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	@Autowired
-	private OrderRepository orderRepository;
+	private OrderRepository orderRepository;	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
---
+++ src/main/java/com/educandoWeb/workshop/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.educandoWeb.workshop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoWeb.workshop.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+
+}
