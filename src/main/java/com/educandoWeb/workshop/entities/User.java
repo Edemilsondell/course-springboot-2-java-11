da5249783cf8c0b1d5f9e1e86223ffc79dafa35d
Edemilson das Mata <edemilsondell@hotmail.com>
2020-05-09 16:09:31 -0400

    Order, Instant, Iso 8601-parte 2

--- src/main/java/com/educandoWeb/workshop/entities/User.java
+++ src/main/java/com/educandoWeb/workshop/entities/User.java
@@ -10,7 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-//import javax.persistence.criteria.Order;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_user")
@@ -26,6 +27,7 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();	
 	
