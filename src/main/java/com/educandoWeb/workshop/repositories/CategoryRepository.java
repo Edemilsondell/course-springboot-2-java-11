d338e7348bfe0352eecb51b8fdbc16fce6cf74c8
Edemilson das Mata <edemilsondell@hotmail.com>
2020-05-09 20:17:09 -0400

    Product

--- src/main/java/com/educandoWeb/workshop/repositories/CategoryRepository.java
+++ src/main/java/com/educandoWeb/workshop/repositories/CategoryRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.educandoWeb.workshop.entities.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
 	
 
 }
---
+++ src/main/java/com/educandoWeb/workshop/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.educandoWeb.workshop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoWeb.workshop.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+
+}
