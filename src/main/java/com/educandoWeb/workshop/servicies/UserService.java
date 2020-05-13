0a245b612db8b92d5159a6e2d5cf81dfd26103aa
Edemilson das Mata <edemilsondell@hotmail.com>
2020-05-13 13:51:25 -0400

    User delete

--- src/main/java/com/educandoWeb/workshop/servicies/UserService.java
+++ src/main/java/com/educandoWeb/workshop/servicies/UserService.java
@@ -30,4 +30,9 @@ public class UserService {
 		return repository.save(obj); //O save já retorna o object salvo por padrão
 	}
 	
+	//Deletando usuário
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
 }
