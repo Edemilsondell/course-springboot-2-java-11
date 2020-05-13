abf577e3abb8c406bed57f6581fbfad0553fd5f4
Edemilson das Mata <edemilsondell@hotmail.com>
2020-05-13 14:11:01 -0400

    User update

--- src/main/java/com/educandoWeb/workshop/resources/UserResource.java
+++ src/main/java/com/educandoWeb/workshop/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,9 +51,18 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
+	//Serve para deletar um usuário no Bd.
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	//Serve para atualizar um usuário no Bd.
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
